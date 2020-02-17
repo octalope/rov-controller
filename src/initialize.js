78c572c63125844f78b4c397ad6266b6dfd024f9
Steve Brooks <steve.c.brooks@gmail.com>
2020-02-16 19:56:23 -0500

    add socket client init and fire events

--- src/initialize.js
+++ src/initialize.js
@@ -1,18 +1,28 @@
 const gamepadController = require('./game-controller');
-const socketEventServer = require('./socket-event-server');
+const SocketEventServer = require('./socket-event-server');
+const SocketEventClient = require('./socket-event-client');
 
-module.exports = (window) => {
-  socketEventServer();
+module.exports = async (window) => {
+  SocketEventServer();
 
   const gamepad = gamepadController(window);
 
-  const display = window.document.querySelector('div[name="gamepadEvents"]');
+  const client = SocketEventClient();
+  client.connect('http://127.0.0.1:3000');
 
-  gamepad.on('gamepadButton', e => {
-    display.innerText = JSON.stringify(e);
-  });
+  await client.registerEvent('gamepadAxis', {broadcast: true});
+  await client.registerEvent('gamepadButton', {broadcast: true});
+
+  const display = window.document.querySelector('div[name="gamepadEvents"]');
 
   gamepad.on('gamepadAxis', e => {
     display.innerText = JSON.stringify(e);
+    client.emitEvent('gamepadAxis', e);
   });
+
+  gamepad.on('gamepadButton', e => {
+    display.innerText = JSON.stringify(e);
+    client.emitEvent('gamepadButton', e);
+  });
+
 };
